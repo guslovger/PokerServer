fa3e222187bb42e986e49b4998b7118402f0bc88
guslucaka <guslucaka@student.gu.se>
2019-01-21 23:12:48 +0100

    Fixat servlet

--- webroot/WEB-INF/classes/servlets/PlayersServlet.java
+++ webroot/WEB-INF/classes/servlets/PlayersServlet.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import java.io.OutputStreamWriter;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.sql.*;
 import java.util.List;
 import org.json.*;
@@ -16,12 +17,33 @@ public class PlayersServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException{
       System.out.println("doPost() " + request);
+      
+      StringBuffer jb = new StringBuffer();
+      JSONObject jsonObject = new JSONObject();
+      String line = null;
+      try {
+          BufferedReader reader = request.getReader();
+        while ((line = reader.readLine()) != null)
+        jb.append(line);
+        } catch (Exception e) { /*report an error*/ }
 
+        try {
+        jsonObject =  HTTP.toJSONObject(jb.toString());
+        } catch (JSONException e) {
+            // crash and burn
+        throw new IOException("Error parsing JSON request string");
+        }
+      
+      System.out.print(jsonObject);
+     
+  }
+    
+    
       // ur rqeuest hämta JSON
       // ur JSON , parsa ut en pokerhand
       // spara pokerhanden i db
       // skicka "OK"
-  }
+    
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
